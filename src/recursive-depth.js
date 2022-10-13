197cc0dc3845ada2dd3b67f86ab48fdace260e2a
snm654 <ruengermany@yandex.by>
2022-10-14 02:49:11 +0300

    feat: recursive-depth not full

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,11 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    function deepCount(a) {
+      let result = 0;
+      for (let i in a) {
+        if (Array.isArray(a[i])) result += 1
+        if (Array.isArray(a[i])) result += deepCount(a[i]);
+      }
+      return result;
+    }
+    return deepCount(arr) + 1
+    
   }
+  
 }
+
+let lol = new DepthCalculator
+
+console.log(lol.calculateDepth([1, 2, 3, 4, [1, 2, [1, 2, [[[]]]]], 5, [1, [[[[[[]]]]]]]]))       // 8
 
 module.exports = {
   DepthCalculator
