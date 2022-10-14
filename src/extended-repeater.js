f3ee685cbeb6f9acf665048f932c43217d53c7e0
snm654 <ruengermany@yandex.by>
2022-10-15 01:16:34 +0300

    feat: extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (typeof str !== 'string') str + ''
+  if (typeof options.addition !== 'string') options.addition + ''
+  if (options.repeatTimes === undefined) options.repeatTimes = 1
+  if (options.separator === undefined) options.separator = '+'
+  if (options.addition === undefined) options.addition = ''
+  if (options.additionSeparator === undefined && (options.addition === 'ADDITION')) options.additionSeparator = '|'
+  if (options.additionSeparator === undefined) options.additionSeparator = ''
+  if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1
+
+  let combo = `${options.addition + options.additionSeparator}`.repeat(options.additionRepeatTimes)
+  combo = combo.slice(0, combo.length - options.additionSeparator.length)
+  str = `${str + combo + options.separator}`.repeat(options.repeatTimes)
+  let slice = str.length - options.separator.length
+  str = str.slice(0, slice)
+
+  return str
 }
+
+// console.log(repeater('TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' })); //TESTstrADD!
+
+
 
 module.exports = {
   repeater
