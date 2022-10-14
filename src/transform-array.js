91d1aba32c23c48c16e446cac3210504793b1122
snm654 <ruengermany@yandex.by>
2022-10-14 20:22:18 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!")
+  if (arr.length === 0) return arr
+  let result = arr.map(el => el)  // закрытие map
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === '--discard-prev' && result[i - 1] !== undefined) {
+      delete result[i - 1]
+    }
+    if (result[i] === '--discard-next' && result[i + 1] !== undefined) {
+      delete result[i + 1]
+    }
+    if (result[i] === '--double-next' && result[i + 1] !== undefined) {
+      result[i] = result[i + 1]
+    }
+    if (result[i] === '--double-prev' && result[i - 1] !== undefined) {
+      result[i] = result[i - 1]
+    }
+  }
+  result = result.filter(el => el !== '--discard-prev')
+  result = result.filter(el => el !== '--discard-next')
+  result = result.filter(el => el !== '--double-next')
+  result = result.filter(el => el !== '--double-prev')
+
+  return result.flat()
 }
+
+
+// console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]));
+//[1, 2, 3, 4, 5]
 
 module.exports = {
   transform
