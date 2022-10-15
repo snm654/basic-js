47ea79f470aa20c671884a4a2b044d6069bf3aa9
snm654 <ruengermany@yandex.by>
2022-10-15 17:59:45 +0300

    feat: +1-2-3

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+if (message === undefined || key === undefined) throw new Error('Incorrect arguments!')
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw new Error('Incorrect arguments!')
   }
 }
 
