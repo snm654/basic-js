4df46d5ac7ad054b92c0c8b2ad5ea736fe7599c0
snm654 <ruengermany@yandex.by>
2022-10-15 04:03:28 +0300

    feat: get email, encode-line,carbon-dating(2/6)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false
+  if (isNaN(sampleActivity)) return false
 }
+
+
+console.log(dateSample('WOOT!'));
 
 module.exports = {
   dateSample
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count = 1;
+  let result = ''
+  if (str === '') return ''
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) result += `${count}${str[i]} `
+    if (str[i] === str[i + 1]) {
+      count++
+    } else {count = 1}
+  }  
+  result = result.split('')
+  return  result.filter(el => el !== '1' && el !== ' ').join('')
 }
 
+
+let a = encodeLine('aabbbc')
+console.log(encodeLine('aabbbc'));
 module.exports = {
   encodeLine
 };
